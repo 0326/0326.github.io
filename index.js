759c7ed9ec01373b5d0f2ced6e538eb02d7b7bd1
0326 <winniringy@gmail.com>
2016-07-02 20:48:29 +0800

    init

---
+++ index.js
@@ -0,0 +1,5 @@
+require('./css/index.less')
+
+// var $test = document.getElementById('test')
+// $test.innerText = 'asdAAAssss324223423433s'
+// console.log('hello....sdfsdf')
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack')
+var path = require('path')
+
+module.exports = {
+    entry: [
+        'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://localhost:8080',
+        path.resolve(__dirname, 'index.js')
+    ],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loader: 'style!css'
+        }, {
+            test: /\.less$/,
+            loader: 'style!css!less'
+        }]
+    }
+}

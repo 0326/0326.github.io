793547c08e8f4f89ac23d1f9b26ef2c3af36b5ec
0326 <winniringy@gmail.com>
2016-07-24 18:01:35 +0800

    add nav section

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 require('./css/index.less')
 
-// var $test = document.getElementById('test')
-// $test.innerText = 'asdAAAssss324223423433s'
-// console.log('hello....sdfsdf')
+console.log('hello....sdfsdf')
